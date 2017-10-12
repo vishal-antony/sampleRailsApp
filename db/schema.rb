5f08ab043b09c57f84ec9b6982cd1137016f601f
Vishal Antony <vishal.antony@freshworks.com>
2017-10-13 00:46:34 +0530

    Add new and edit button for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008172646) do
+ActiveRecord::Schema.define(version: 20161228234626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blogs", force: :cascade do |t|
+  create_table "blogs", id: :serial, force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "portfolios", force: :cascade do |t|
+  create_table "portfolios", id: :serial, force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
     t.text "body"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20171008172646) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "skills", force: :cascade do |t|
+  create_table "skills", id: :serial, force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
